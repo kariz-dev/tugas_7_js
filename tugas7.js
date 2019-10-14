73ea8012fa0c7357424bc5a5899a365703c9a0ab
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-14 23:51:41 +0700

    tugas 7 javascript

---
+++ tugas7.js
@@ -0,0 +1,15 @@
+/* tugas 7 javascript */
+// Muhammad Rizwan
+
+var tinggiBadan = ["160cm","165cm","170cm","175cm","180cm","185cm"];
+
+// looping array
+for(var i=0; i<tinggiBadan.length; i++){
+    console.log(tinggiBadan[i]);
+}
+
+// for of
+for(let y of tinggiBadan){
+    console.log(y);
+}
+
